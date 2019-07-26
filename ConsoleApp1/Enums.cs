358a12e2437b1f91110c6d03a67bdb324a8ffe4e
JessytheKing <jesus.a.estefo@outlook.com>
2019-07-26 10:34:54 -0700

    Changed name

---
+++ ConsoleApp1/Enums.cs
@@ -0,0 +1,19 @@
+﻿
+class Enums
+
+{
+    enum Rank { Private = 1, PFC = 2, LCPL = 3, CPL = 4, SGT = 5, SSGT = 6, GUNNY = 7, FIRSTSGT = 8, MASTERSGT = 9, SGTMAJOR = 10, MASTERGUNNERYSGT = 10 }
+    static void Main(string[] args)
+    {
+        int CPL = (int)Rank.CPL;
+        int SGT = (int)Rank.SGT;
+
+        System.Console.WriteLine($"Jesus's rank makes up the backbone of the Marine Corps an E-{CPL}");
+        System.Console.WriteLine($"Jacob is an E-{SGT} ");
+        
+
+    }
+
+    
+
+}
